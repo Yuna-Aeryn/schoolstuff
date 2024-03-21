83a9039e6aa86e153a678b8b3a1466c219583682
Yuna-Aeryn <choiyoon011@gmail.com>
2024-03-21 16:55:03 +0900

    checkup on things

--- Data Structures/anything.c
+++ Data Structures/anything.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
  
 int main() {
-  int myNumbers[4] = {25, 50, 75, 100};
+  int myNumbers[4] = {36, 50, 75, 100};
 
   // Get the value of the second element in myNumbers
   printf("%d\n", *(myNumbers + 1));
