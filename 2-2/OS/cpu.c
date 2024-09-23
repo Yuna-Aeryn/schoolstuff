50536eac181246d859e5c1ba175a381709c91d1b
Yuna_Aeryn <choiyoon011@outlook.kr>
2024-09-23 16:46:43 +0900

    generic

--- 2-2/OS/cpu.c
+++ 2-2/OS/cpu.c
@@ -4,7 +4,7 @@
 #include <assert.h>
 #include <windows.h>
 
-int main(int argc, char *argv[])
+public static void int main(int argc, char *argv[])
 {
     if (argc != 2) { 
         fprintf(stderr, "usage: cpu <string>\n");
