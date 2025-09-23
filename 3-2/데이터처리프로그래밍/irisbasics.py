776aaeb2940c5ee1c46ff6279981c84e12c0ccad
Yuna_Aeryn <choiyoon011@outlook.kr>
2025-09-23 15:12:32 +0900

    standard

--- 3-2/데이터처리프로그래밍/irisbasics.py
+++ 3-2/데이터처리프로그래밍/irisbasics.py
@@ -1,9 +1,8 @@
 import pandas as pd
-import seaborn as sns
-iris = sns.load_dataset("iris")
+df = pd.DataFrame({'상품' : ['시계', '반지', '반지', '목걸이', '팔찌'],
+'재질' : ['금', '은', '백금', '금', '은'],
+'가격': [500000, 20000, 350000, 300000, 60000]})
 
 
-
-new_iris = iris.drop(2, axis=0)
-new_iris = iris.drop([1, 2])
-new_iris.head()
+new_df = df.pivot(index='상품', columns='재질', values='가격')
+print(new_df.fillna(value=0))
